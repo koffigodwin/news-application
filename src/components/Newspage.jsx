64a56ad9068d348c783804e915badf3c195d9baf
unknown <djogbemakoffigodwin@gmail.com>
2024-05-13 14:33:02 +0530

    first commit

--- src/components/Newspage.jsx
+++ src/components/Newspage.jsx
@@ -1,8 +1,6 @@
 import React, { useState,useEffect } from 'react'
 import Eachnew from './Eachnew'
 
-
-
 const Newspage = ({category}) => {
     const [dataapi,setDataapi] = useState([])
     useEffect(()=>{
@@ -10,15 +8,14 @@ const Newspage = ({category}) => {
         const Url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_APP_KEY}`
         
         fetch(Url).then(response => response.json()).then(data => setDataapi(data.articles))
-    },[category])
-
-    
+     },[category])
+   
   return (
     <div className="all">
         <h2>Latest <strong className='rednews'>News</strong></h2>
         <div className='Card-container'>
         {
-            dataapi.map((news,index) =>{
+            dataapi?.map((news,index) =>{
              const {author,content,description,title,url,urlToImage} = news
              return <Eachnew  key={index} author={author} content={content} description={description} title={title} url={url} src={urlToImage}/>
             })
